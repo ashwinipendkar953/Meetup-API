70f4516e084aac6641a2ac3addf87351bfd5028d
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2024-06-19 19:37:26 +0530

    feat: added event routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,8 +2,44 @@ const express = require("express");
 const router = express.Router();
 const Event = require("../models/events.models");
 
-router.get("/events", (req, res) => {
-  res.send("Event Routes Page");
+// add event
+router.post("/events", async (req, res) => {
+  try {
+    const newEvent = new Event(req.body);
+    await newEvent.save();
+    res
+      .status(200)
+      .send({ message: "Event added successfully", event: newEvent });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+// get events
+router.get("/events", async (req, res) => {
+  try {
+    const events = await Event.find();
+    if (events) {
+      res.status(200).send(events);
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+// get event by id
+router.get("/events/:eventId", async (req, res) => {
+  try {
+    const eventId = req.params.eventId;
+    const event = await Event.findById(eventId);
+    if (event) {
+      res.status(200).send(event);
+    } else {
+      res.status(404).send({ error: "Event not found." });
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
